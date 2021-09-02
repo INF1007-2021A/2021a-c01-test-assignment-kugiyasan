c821a79e118245c874dd6272554c5b9d7957e715
kugiyasan <kugiyasan@users.noreply.github.com>
2021-09-02 14:27:16 -0400

    Edited the text to print in exercice.py

--- exercice.py
+++ exercice.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-if __name__ == '__main__':
-    #TODO: Modifier le code ici
-    print("Le cours INF1007 est mon cours")
+if __name__ == "__main__":
+    print("Le cours INF1007 est mon cours!")
